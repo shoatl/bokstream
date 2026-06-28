26f9771c8273feb28ebaa3f3287853de2c37ccef
shoatl <albertojrgalicia05@gmail.com>
2026-06-28 17:30:49 +0800

    Update API key and fix formatting issues

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '9871bc59ef6f90d08eec9c49639c6151';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
